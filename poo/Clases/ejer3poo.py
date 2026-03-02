a38dbaca0e175002f42e1145e21064a40b0a3e1d
Mateo Navarro <878.mateo@gmail.com>
2026-03-02 09:41:49 -0600

    Version 2 02/03/2026

---
+++ poo/Clases/ejer3poo.py
@@ -0,0 +1,11 @@
+import math 
+class Ejercicio3():
+    def __init__(self):
+        while True:
+            self.palabra = input("palabra:")
+            if self.palabra == "salir":
+                break
+            else:
+                print(self.palabra)
+    
+  
